d1ac4a21ec9025a6a2b23bb3ac8ff5fe47fd15b8
yogeshwar1996 <yogeshwartrehan@gmail.com>
2024-06-22 16:16:11 +0530

    Add toggle button and sidebar to other pages

--- Practice/Navigation Bar and Plans Practice 3/shared.js
+++ Practice/Navigation Bar and Plans Practice 3/shared.js
@@ -5,32 +5,32 @@ var modalNoButton = document.querySelector(".modal button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 
-for (var i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener("click", function () {
+for (var i = 0; i < selectPlanButtons?.length; i++) {
+  selectPlanButtons[i]?.addEventListener("click", function () {
     // backdrop.style.display = "block";
     // modal.style.display = "none";
-    backdrop.classList.add("open");
-    modal.classList.add("open");
+    backdrop?.classList.add("open");
+    modal?.classList.add("open");
   });
 }
 
-backdrop.addEventListener("click", function () {
+backdrop?.addEventListener("click", function () {
   // mobileNav.style.display = "none";
-  mobileNav.classList.remove("open");
+  mobileNav?.classList.remove("open");
   closeModal();
 });
-modalNoButton.addEventListener("click", closeModal);
+modalNoButton?.addEventListener("click", closeModal);
 
 function closeModal() {
   // backdrop.style.display = "none";
   // modal.style.display = "none";
-  backdrop.classList.remove("open");
-  modal.classList.remove("open");
+  backdrop?.classList.remove("open");
+  modal?.classList.remove("open");
 }
 
 toggleButton.addEventListener("click", function () {
   // mobileNav.style.display = "block";
   // backdrop.style.display = "block";
-  mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  mobileNav?.classList.add("open");
+  backdrop?.classList.add("open");
 });
