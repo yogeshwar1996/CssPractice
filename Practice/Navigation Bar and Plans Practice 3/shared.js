3731a5f8b702e8d01bcf7e60f2f90e838d75715f
yogeshwar1996 <yogeshwartrehan@gmail.com>
2024-06-22 15:24:40 +0530

    Understading modals and backdrop and navigation menu

--- Practice/Navigation Bar and Plans Practice 3/shared.js
+++ Practice/Navigation Bar and Plans Practice 3/shared.js
@@ -7,23 +7,30 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.style.display = "none";
+    backdrop.classList.add("open");
+    modal.classList.add("open");
   });
 }
 
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
   closeModal();
 });
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  backdrop.classList.remove("open");
+  modal.classList.remove("open");
 }
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.classList.add("open");
 });
